39ece6f1f6908053fa6d1c9555458ce064f2b72c
Pablo <pabrodez@gmail.com>
2020-08-07 15:24:53 +0100

    Remove unused imports

--- src/main/java/GolfNowLoader.java
+++ src/main/java/GolfNowLoader.java
@@ -1,16 +1,10 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class GolfNowLoader {
 
